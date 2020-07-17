19975f469b74c95a03f5e5167766e92888eff23c
mamatha67 <63779669+mamatha67@users.noreply.github.com>
2020-07-17 13:51:41 +0530

    Update codeefs.tf

--- codeefs.tf
+++ codeefs.tf
@@ -142,3 +142,38 @@ resource "null_resource" "nullremote1" {
     ]
   }
 }
+resource "aws_cloudfront_distribution" "cloudfront1" {
+    enabled             = true
+    is_ipv6_enabled     = true
+    wait_for_deployment = false
+    origin {
+        domain_name = "${aws_s3_bucket.b.bucket_regional_domain_name}"
+        origin_id   = local.s3_origin_id
+        
+}
+    default_cache_behavior {
+        allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+        cached_methods = ["GET", "HEAD"]
+        target_origin_id = local.s3_origin_id
+      forwarded_values {
+            query_string = false
+        
+            cookies {
+               forward = "none"
+            }
+        }
+        
+        viewer_protocol_policy = "redirect-to-https"
+        min_ttl                =  0
+        default_ttl            =  3600
+        max_ttl                =  86400
+    }
+    restrictions {
+        geo_restriction {
+            restriction_type = "none"
+        }
+    }
+viewer_certificate {
+        cloudfront_default_certificate = true
+    }
+}
